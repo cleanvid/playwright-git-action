b9ef6c2f42bc21bfb639ed6a919e95a70ce497ae
unknown <sophek@gmail.com>
2023-08-25 08:26:08 -0500

    another push to repo

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -17,14 +17,14 @@ test('get started link', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
-// test('should see Installing Playwright heading', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
+test('should see Installing Playwright heading', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
 
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
 
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installing Playwright' })).toBeVisible();
-// });
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installing Playwright' })).toBeVisible();
+});
 
 
